1164cabedf91adddc49a6b942f3820f8bfd816d9
michelmoser <michel.moser@ips.unibe.ch>
2018-06-07 16:34:04 +0200

    qtl2abh and back

--- Rqtl_2_abh.py
+++ Rqtl_2_abh.py
@@ -1,82 +1,151 @@
 #conversion of asmap-output csv to abh-genotyper csv input
 import sys
 from itertools import chain
+import argparse
 
 
 
-INFILE=sys.argv[1]
-OUTFILE=sys.argv[2]
+
+parser = argparse.ArgumentParser(description='Convert between qtl and abh input')
 
 
-header = []
-LG = []
-CM = []
-chrs = []
-body = []
+parser.add_argument('-q2a', default=False, help='convert qtl csv-file to abhGenotyper csv-file', action='store_true')
 
-with open(INFILE, "r") as infi: 
-    line = ""
-    for k, line in enumerate(infi): 
-        k += 1
-        #marker
-        if k == 1: 
+parser.add_argument('-a2q', default=False, help='convert abhGenotyper csv-file to qtl csv-file', action='store_true')
+
+parser.add_argument('-i', '--input',  help='specify input csv-file', required =
+                   True)
+parser.add_argument('-o', '--output', help='specify output csv-file', required
+                   = True)
+
+args = parser.parse_args()
+
+
+
+INFILE=args.input
+OUTFILE=args.output
+
+
+def qtl2abh(csvfile): 
+    
+    header = []
+    LG = []
+    CM = []
+    chrs = []
+    body = []
+    
+    with open(INFILE, "r") as infi: 
+        line = ""
+        for k, line in enumerate(infi): 
+            k += 1
+            #marker
+            if k == 1: 
+                
+                #generate header line in manner of 
+                #[genotypes,marker1Scaffold1_positionL#n_CM, marker2Scaffold1_position#n_CM....]
+                #use centimorgans to generate distance across LGs
+                line = line.strip()
+                line = line.split(',')
+                line =  [it.replace("_","bp") for it in line]
+                header = line
+                
+            #LGs
+            elif k == 2: 
+                line = line.strip()
+                line = line.split(',')
+                line =  [it.replace(".","") for it in line]
+                LG = line
+                for ak in line: 
+                    ak1 = ak.replace("L","")
+                    chrs.append(ak1)
+            #CM
+            elif k == 3:
+                line = line.strip()
+                line = line.split(",")
+                kl = [] 
+                for i in line: 
+                    if i:
+                        kl.append(str(int(float(i)* 100000)))
+                    else: 
+                        kl.append("")#print(str(i))
+                CM= kl
             
-            #generate header line in manner of 
-            #[genotypes,marker1Scaffold1_positionL#n_CM, marker2Scaffold1_position#n_CM....]
-            #use centimorgans to generate distance across LGs
-            line = line.strip()
-            line = line.split(',')
-            line =  [it.replace("_","bp") for it in line]
-            header = line
-            
-        #LGs
-        elif k == 2: 
-            line = line.strip()
-            line = line.split(',')
-            line =  [it.replace(".","") for it in line]
-            LG = line
-            for ak in line: 
-                ak1 = ak.replace("L","")
-                chrs.append(ak1)
-        #CM
-        elif k == 3:
-            line = line.strip()
-            line = line.split(",")
-            kl = [] 
-            for i in line: 
-                if i:
-                    kl.append(str(int(float(i)* 100000)))
-                else: 
-                    kl.append("")#print(str(i))
-            CM= kl
-        
-        else:
-            #print(line)
-            line = line.strip()
-            line = line.split(",")
-            line =  [it.replace("AA","A") for it in line]
-            line =  [it.replace("BB","B") for it in line]
-            line =  [it.replace("-","N") for it in line]
-            body.append(line)
+            else:
+                #print(line)
+                line = line.strip()
+                line = line.split(",")
+                line =  [it.replace("AA","A") for it in line]
+                line =  [it.replace("BB","B") for it in line]
+                line =  [it.replace("-","N") for it in line]
+                body.append(line)
+    
+    
+    gz = []
+    for ei in list(zip(header, LG)):
+       gz.append("".join(ei))
+    
+    gz1 = []
+    for eii in list(zip(gz, CM)):
+         gz1.append("_".join(eii))
+    
+    
+    gz1[0] = "genotypes"
+    chrs[0] = "chrs"
+    
+    print("Extracting {} markers from {} individuals".format(len(gz1)-1, k-3))
+    
+    with open(OUTFILE, "w") as outf:
+        outf.write(",".join(gz1)+"\n")
+        outf.write(",".join(chrs)+"\n")
+        for li in body:
+            outf.write(",".join(li)+"\n")
+    print("Finished.")
 
 
-gz = []
-for ei in list(zip(header, LG)):
-   gz.append("".join(ei))
+def abh2qtl(csvfile): 
+    
+    harvest = []
 
-gz1 = []
-for eii in list(zip(gz, CM)):
-     gz1.append("_".join(eii))
+    with open(INFILE, "r") as infi: 
+        line = ""
+        for k, line in enumerate(infi): 
+            k += 1
+            #marker
+            if k == 1:
+                line = line.strip()
+                line = line.split(",")
+                line[0] ==  "Genotype"
+                harvest.append(line)
+
+            elif k == 2:
+                line = line.strip()
+                line = line.split(",")
+                line[0] = ""
+                harvest.append(line)
+            else: 
+                line = line.strip()
+                line = line.split(",")
+                harvest.append(line)
+
+    with open(OUTFILE, "w") as outf: 
+        for li in harvest: 
+            outf.write(",".join(li)+"\n")
+        print("Finished.")
 
 
-gz1[0] = "genotypes"
-chrs[0] = "chrs"
+def main():
 
-print("Extracting {} markers from {} individuals".format(len(gz1), k-3))
+    if args.q2a: 
+        qtl2abh(args.input)
+        print("QTL to ABHGENOTYPER")
 
-with open(OUTFILE, "w") as outf:
-    outf.write(",".join(gz1)+"\n")
-    outf.write(",".join(chrs)+"\n")
-    for li in body:
-        outf.write(",".join(li)+"\n")
-print("Finished.")
+    elif args.a2q: 
+       abh2qtl(args.input)
+       print("ABHGENOTYPER to QTL")
+
+    else: 
+        print("Nothing to do... type -h for options")
+
+
+if __name__ == "__main__":
+       main()
